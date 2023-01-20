933efb7db2f5b27188ce8ff7218f0ab1259b353c
KwamesKorner <kwame.asante@kxtgconsulting.com>
2023-01-20 17:00:19 -0500

    config for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
   optimization: {
