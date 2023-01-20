a437c9cbfceab3b03f2aab4f3fd02ab7b10ed1fa
KwamesKorner <kwame.asante@kxtgconsulting.com>
2023-01-20 15:37:35 -0500

    adding google analytics

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { analytics } from 'react-analytics'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <analytics id="G-MRJNYMVTTC">
+      <App />
+    </analytics>
   </React.StrictMode>
 );
 
