c58a9772c087c057fe00150e87b7e64a0125b33f
KwamesKorner <kwame.asante@kxtgconsulting.com>
2023-01-21 01:56:11 -0500

    webpack further config

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,66 @@
 const path = require('path');
-const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.join(__dirname, '/dist'),
+    filename: 'index_bundle.js'
   },
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin()],
-  },
+  mode: 'production',
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-        },
+          loader: 'babel-loader'
+        }
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       },
       {
-        test: /\.(png|jpg|gif)$/,
+        test: /\.(gif|png|jpe?g|svg)$/i,
         use: [
+          'file-loader',
           {
-            loader: 'file-loader',
+            loader: 'image-webpack-loader',
             options: {
-              name: '[path][name].[ext]',
+              bypassOnDebug: true,
             },
           },
         ],
       }
-            
-    ],
+    ]
   },
-};
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css'
+    }),
+    new webpack.ProvidePlugin({
+        process: 'process/browser',
+    }),
+    new WebpackPwaManifest({
+        filename: 'manifest.json',
+        name: 'My PWA',
+        short_name: 'My PWA',
+        description: 'My Progressive Web App',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: path.resolve('public/apple-touch-icon.png'),
+            sizes: [128]
+          }
+        ]
+      })
+  ]
+}
