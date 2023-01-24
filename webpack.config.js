e9678e5b635d69a8cb06bf82d61aa2429ed6d314
KwamesKorner <kwame.asante@kxtgconsulting.com>
2023-01-24 00:58:42 -0500

    config changes

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Dotenv = require("dotenv-webpack");
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = {
@@ -13,54 +14,47 @@ module.exports = {
   mode: 'production',
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
+        {
+            test: /\.js$/,
+            use: 'babel-loader',
+            exclude: /node_modules/
+        },
+        {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        },
+        {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: 'file-loader'
         }
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              bypassOnDebug: true,
-            },
-          },
-        ],
-      }
     ]
   },
   plugins: [
+    new Dotenv({
+        systemvars: true
+    }),
     new HtmlWebpackPlugin({
-      template: './public/index.html'
+        template: './public/index.html'
     }),
     new MiniCssExtractPlugin({
-      filename: 'App.css'
+        filename: 'App.css'
     }),
     new webpack.ProvidePlugin({
         process: 'process/browser',
     }),
-    new WebpackPwaManifest({
-        filename: 'manifest.json',
-        name: 'My PWA',
-        short_name: 'My PWA',
-        description: 'My Progressive Web App',
-        background_color: '#ffffff',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: path.resolve('public/apple-touch-icon.png'),
-            sizes: [128]
-          }
-        ]
-      })
+    // new WebpackPwaManifest({
+    //     filename: 'manifest.json',
+    //     name: 'My PWA',
+    //     short_name: 'My PWA',
+    //     description: 'My Progressive Web App',
+    //     background_color: '#ffffff',
+    //     theme_color: '#ffffff',
+    //     icons: [
+    //       {
+    //         src: path.resolve('public/apple-touch-icon.png'),
+    //         sizes: [128]
+    //       }
+    //     ]
+    //   })
   ]
 }
